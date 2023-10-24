9a5aceec37f27c06d2dfdcc9933bbe4315293894
osichan <oskarsajenko@gmail.com>
2023-10-24 13:33:25 +0300

    added lab 3

---
+++ lab_3/main.py
@@ -0,0 +1,33 @@
+class BinaryTree:
+    def __init__(self, value, left=None, right=None, children=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.children = children
+
+
+def inorder_traversal(node):
+    values = []
+    if node is not None:
+        values += inorder_traversal(node.left)
+        values.append(node)
+        values += inorder_traversal(node.right)
+    return values
+
+
+def find_successor(tree: BinaryTree, node: BinaryTree) -> BinaryTree:
+    inorder_array = inorder_traversal(tree)
+    for i in range(len(inorder_array)):
+        if inorder_array[i].value == node.value:
+            if i < len(inorder_array) - 1:
+                return inorder_array[i + 1]
+    return BinaryTree("Not Found")
+
+
+root = BinaryTree(10)
+root.left = BinaryTree(5, BinaryTree(3), BinaryTree(7))
+root.right = BinaryTree(15, None, BinaryTree(20, BinaryTree(12)))
+node_to_find_successor = root.left.right  # Вершина зі значенням 7
+
+successor = find_successor(root, node_to_find_successor)
+print(successor.value)
